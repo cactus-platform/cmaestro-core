cab031aeddf2b955d562d3b43105fd0bbf368349
qerdinger <91551241+QuentinCraft@users.noreply.github.com>
2026-08-28 08:08:24 +0200

    feat: adds models foundation

---
+++ cmaestro-core/models/artifact.go
@@ -0,0 +1,25 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Artifact struct {
+	Id uuid.UUID `json:"id" gorm:"primaryKey"`
+
+	Name string `json:"name" gorm:"unique"`
+	Path string `json:"path" gorm:"unique"`
+
+	Revision string `json:"revision"`
+
+	Hash   string `json:"hash"`
+	Size   int64  `json:"size"`
+	Format string `json:"format"`
+
+	Status string `json:"status"`
+
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}

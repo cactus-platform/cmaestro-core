0ea94b7d33c055b236b3f299cc7e2e1636ce65c1
qerdinger <91551241+QuentinCraft@users.noreply.github.com>
2026-09-20 11:31:36 +0200

    feat: adds Get/Delete methods in Ingest Svc

--- services/ingest_service.go
+++ services/ingest_service.go
@@ -27,6 +27,9 @@ func (s *IngestServiceImpl) Ingest(ctx context.Context, repository *models.Repos
 	if s.keyVal == nil {
 		return errors.New("key-val client cannot be nil")
 	}
+	if repository == nil {
+		return errors.New("repository cannot be nil")
+	}
 
 	if err := ctx.Err(); err != nil {
 		return err
@@ -53,4 +56,79 @@ func (s *IngestServiceImpl) Ingest(ctx context.Context, repository *models.Repos
 		string(value),
 		-1,
 	)
+}
+
+func (s *IngestServiceImpl) Update(ctx context.Context, repository *models.Repository, status models.IngestStatus) error {
+	if s.keyVal == nil {
+		return errors.New("key-val client cannot be nil")
+	}
+	if repository == nil {
+		return errors.New("repository cannot be nil")
+	}
+
+	if err := ctx.Err(); err != nil {
+		return err
+	}
+
+	if len(repository.Artifacts) == 0 {
+		return errors.New("no artifacts defined")
+	}
+
+	now := time.Now().UTC()
+	value, err := json.Marshal(models.Ingest{
+		RepositoryID: repository.ID,
+		Revision:     repository.Artifacts[0].ID,
+		Status:       status,
+		UpdatedAt:    now,
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return s.keyVal.Set(
+		"ingest:"+repository.ID.String(),
+		string(value),
+		-1,
+	)
+}
+
+func (s *IngestServiceImpl) Delete(ctx context.Context, repository *models.Repository) error {
+	if s.keyVal == nil {
+		return errors.New("key-val client cannot be nil")
+	}
+	if repository == nil {
+		return errors.New("repository cannot be nil")
+	}
+
+	if err := ctx.Err(); err != nil {
+		return err
+	}
+
+	return s.keyVal.Delete("ingest:" + repository.ID.String())
+}
+
+func (s *IngestServiceImpl) Get(ctx context.Context, repository *models.Repository) (*models.Ingest, error) {
+	if s.keyVal == nil {
+		return nil, errors.New("key-val client cannot be nil")
+	}
+	if repository == nil {
+		return nil, errors.New("repository cannot be nil")
+	}
+
+	if err := ctx.Err(); err != nil {
+		return nil, err
+	}
+
+	value, err := s.keyVal.Get("ingest:" + repository.ID.String())
+	if err != nil {
+		return nil, err
+	}
+
+	var ingest models.Ingest
+	if err := json.Unmarshal([]byte(value), &ingest); err != nil {
+		return nil, err
+	}
+
+	return &ingest, nil
 }
